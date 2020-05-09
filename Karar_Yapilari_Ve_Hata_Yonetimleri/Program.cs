ebde7201ec0407912117d684cf6f04a896b613fe
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-09 22:54:10 +0300

    Initial commit

---
+++ Karar_Yapilari_Ve_Hata_Yonetimleri/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fiyat = 0;
+            Console.Write("RESTORAN\n\nPilav: 5 TL, İskender: 10 TL\n\nYemek tercihi yapınız: ");
+            string tercih = Console.ReadLine();
+            if (tercih == "pilav" || tercih == "Pilav" || tercih == "PİLAV")
+            {
+                try
+                {
+                    fiyat += 5;
+                    Console.Write("\nKaç adet istersiniz: ");
+                    int adet = Convert.ToInt32(Console.ReadLine());
+                    fiyat = fiyat * adet;
+                    Console.Write("Toplam ücretiniz: " + fiyat + " TL");
+                }
+                catch (FormatException)
+                {
+                    Console.Write("\nBir adet girmeliydiniz.");
+                }
+            }
+            else if (tercih == "iskender" || tercih == "İskender" || tercih == "İSKENDER")
+            {
+                try
+                {
+                    fiyat += 10;
+                    Console.Write("\nKaç adet istersiniz: ");
+                    int adet = Convert.ToInt32(Console.ReadLine());
+                    fiyat = fiyat * adet;
+                    Console.Write("\nToplam ücretiniz: " + fiyat + " TL");
+                }
+                catch (FormatException)
+                {
+                    Console.Write("\nBir adet girmeliydiniz.");
+                }
+            }
+            else
+            {
+                Console.Write("\nMenüde böyle bir yemek bulunmamaktadır.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Exception-Handling-With-Conditional-Statements
+ Koşullu ifadeler ile hata yakalama. / Exception handling with conditional statements.
